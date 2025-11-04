6e5d904d3142b8798164bb504670d4cc5fdc2394
itsme-shyam-702 <sssshyam702@gmail.com>
2025-11-04 17:51:21 +0530

    Fixed double router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -12,31 +11,28 @@ import AdmissionDashboard from "./pages/AdmissionDashboard";
 import InboxDashboard from "./pages/InboxDashboard";
 import { SignIn } from "@clerk/clerk-react";
 
-
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/admissions" element={<Admissions />} />
-          <Route path="/dashboard" element={<AdmissionDashboard />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/inbox" element={<InboxDashboard />} />
-        
-          <Route path="/login" element={<SignIn />} />
-        </Routes>
-      </div>
-    <Footer/>
-    </Router>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/admissions" element={<Admissions />} />
+        <Route path="/dashboard" element={<AdmissionDashboard />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/inbox" element={<InboxDashboard />} />
+        <Route path="/login" element={<SignIn />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
 
 
 // https://jr-school-67nt.onrender.com
